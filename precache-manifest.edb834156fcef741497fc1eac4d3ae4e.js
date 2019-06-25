4d36476c8bfddf8c52fb8ee81e5f233607979cb9
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2019-06-25 04:02:26 -0300

    Updates

---
+++ precache-manifest.edb834156fcef741497fc1eac4d3ae4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9c8a01744404dea836eff26297e9e6",
+    "url": "/admin-portifolio/index.html"
+  },
+  {
+    "revision": "4271d46de53a248ebff1",
+    "url": "/admin-portifolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0f5c9791878eca41f081",
+    "url": "/admin-portifolio/static/css/main.3f46bffd.chunk.css"
+  },
+  {
+    "revision": "4271d46de53a248ebff1",
+    "url": "/admin-portifolio/static/js/2.54c407f1.chunk.js"
+  },
+  {
+    "revision": "0f5c9791878eca41f081",
+    "url": "/admin-portifolio/static/js/main.9df91788.chunk.js"
+  },
+  {
+    "revision": "d3e9b74afd6899ba7abb",
+    "url": "/admin-portifolio/static/js/runtime~main.fb090b36.js"
+  }
+]);
